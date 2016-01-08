d042753cd9c14b5ca025a9bac8c42abe3ed34ff5
Ramon Ferreira Silva <rsilva@ramonsilva.net>
2016-01-08 16:56:43 -0200

    Refletor do method openPreferredLocationInMap

--- app/src/main/java/net/ramonsilva/sunshine/app/MainActivity.java
+++ app/src/main/java/net/ramonsilva/sunshine/app/MainActivity.java
@@ -58,14 +58,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void openPreferredLocationInMap() {
-        SharedPreferences sharedPrefs =  PreferenceManager.getDefaultSharedPreferences(this);
-        String location = sharedPrefs.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
+        final String location = Utility.getPreferredLocation(this);
 
-        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+        final Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
                 .appendQueryParameter("q", location)
                 .build();
 
-        Intent intent = new Intent(Intent.ACTION_VIEW);
+        final Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(geoLocation);
 
         if (intent.resolveActivity(getPackageManager()) != null) {
