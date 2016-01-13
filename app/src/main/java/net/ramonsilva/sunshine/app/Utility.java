e770891e3e9d0aa842afcf2a4a94443851f8be52
Ramon Ferreira Silva <rsilva@ramonsilva.net>
2016-01-13 17:07:51 -0200

    Alteração de Layout
    Alteração do List Item xml
    Criação do list item today
    Alteracao Forecast Adapter

--- app/src/main/java/net/ramonsilva/sunshine/app/Utility.java
+++ app/src/main/java/net/ramonsilva/sunshine/app/Utility.java
@@ -3,14 +3,22 @@ package net.ramonsilva.sunshine.app;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.text.format.Time;
 
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
  * Created by ramonsilva on 08/01/16.
  */
 public class Utility {
+
+    // Format used for storing dates in the database.  ALso used for converting those strings
+    // back into date objects for comparison/processing.
+    public static final String DATE_FORMAT = "yyyyMMdd";
+
+
     public static String getPreferredLocation(Context context) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         return prefs.getString(context.getString(R.string.pref_location_key),
@@ -37,5 +45,56 @@ public class Utility {
     public static String formatDate(long dateInMillis) {
         Date date = new Date(dateInMillis);
         return DateFormat.getDateInstance().format(date);
+    }
+
+    public static String getFriendlyDayString(Context context, long dateInMillis) {
+        final Time time = new Time();
+        time.setToNow();
+        final long currentTime = System.currentTimeMillis();
+        final int julianDay = Time.getJulianDay(dateInMillis, time.gmtoff);
+        final int currentJulianDay = Time.getJulianDay(currentTime, time.gmtoff);
+
+        if (julianDay == currentJulianDay) {
+            final String today = context.getString(R.string.today);
+            final int formatId = R.string.format_full_friendly_date;
+            return String.format(context.getString(
+                    formatId,
+                    today,
+                    getFormattedMonthDay(context, dateInMillis)));
+        } else if ( julianDay < currentJulianDay + 7 ) {
+            return getDayName(context, dateInMillis);
+        } else {
+            final SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
+            return shortenedDateFormat.format(dateInMillis);
+        }
+    }
+
+    public static String getDayName(Context context, long dateInMillis) {
+        // If the date is today, return the localized version of "Today" instead of the actual
+        // day name.
+
+        final Time t = new Time();
+        t.setToNow();
+        final int julianDay = Time.getJulianDay(dateInMillis, t.gmtoff);
+        final int currentJulianDay = Time.getJulianDay(System.currentTimeMillis(), t.gmtoff);
+        if (julianDay == currentJulianDay) {
+            return context.getString(R.string.today);
+        } else if ( julianDay == currentJulianDay +1 ) {
+            return context.getString(R.string.tomorrow);
+        } else {
+            Time time = new Time();
+            time.setToNow();
+            // Otherwise, the format is just the day of the week (e.g "Wednesday".
+            final SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE");
+            return dayFormat.format(dateInMillis);
+        }
+    }
+
+    public static String getFormattedMonthDay(Context context, long dateInMillis ) {
+        final Time time = new Time();
+        time.setToNow();
+        final SimpleDateFormat monthDayFormat = new SimpleDateFormat("MMMM dd");
+        final String monthDayString = monthDayFormat.format(dateInMillis);
+        return monthDayString;
     }
 }
--- app/src/main/java/net/ramonsilva/sunshine/app/data/ForecastAdapter.java
+++ app/src/main/java/net/ramonsilva/sunshine/app/data/ForecastAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import net.ramonsilva.sunshine.app.ForecastFragment;
@@ -17,7 +18,10 @@ import net.ramonsilva.sunshine.app.Utility;
  */
 public class ForecastAdapter extends CursorAdapter {
 
-   private Context mContext;
+    private Context mContext;
+
+    private static final int VIEW_TYPE_TODAY  = 0;
+    private static final int VIEW_TYPE_FUTURE_DAY  = 1;
 
     public ForecastAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
@@ -26,14 +30,57 @@ public class ForecastAdapter extends CursorAdapter {
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        final View view = LayoutInflater.from(mContext).inflate(R.layout.list_item_forecast, parent, false);
+
+        final int viewType = getItemViewType(cursor.getPosition());
+        int layoutId = -1;
+
+        if(VIEW_TYPE_TODAY == viewType){
+            layoutId = R.layout.list_item_forescast_today;
+        } else {
+            layoutId = R.layout.list_item_forecast;
+        }
+
+        final View view = LayoutInflater.from(mContext).inflate(layoutId, parent, false);
         return view;
     }
 
     @Override
+    public int getItemViewType(int position) {
+        return (position == 0) ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
+    @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        final TextView txView = (TextView) view;
-        txView.setText(convertCursorRowToUXFormat(cursor));
+
+        final int weatherId = cursor.getInt(ForecastFragment.COL_WEATHER_ID);
+        final ImageView iconView = (ImageView) view.findViewById(R.id.list_item_icon);
+        iconView.setImageResource(R.mipmap.ic_launcher);
+
+        final String forecast = cursor.getString(ForecastFragment.COL_WEATHER_DESC);
+        final TextView forecastView = (TextView) view.findViewById(R.id.list_item_forecast_textview);
+        forecastView.setText(forecast);
+
+        final String date = Utility.getFriendlyDayString(context, cursor.getLong(ForecastFragment.COL_WEATHER_DATE));
+        final TextView dateView = (TextView) view.findViewById(R.id.list_item_date_textview);
+        dateView.setText(date);
+
+
+        final boolean isMetric = Utility.isMetric(context);
+
+        final double high = cursor.getDouble(ForecastFragment.COL_WEATHER_MAX_TEMP);
+        final TextView highView = (TextView) view.findViewById(R.id.list_item_high_textview);
+        highView.setText(Utility.formatTemperature(high, isMetric));
+
+        final double low = cursor.getDouble(ForecastFragment.COL_WEATHER_MAX_TEMP);
+        final TextView lowView = (TextView) view.findViewById(R.id.list_item_low_textview);
+        lowView.setText(Utility.formatTemperature(low, isMetric));
+
+
     }
 
 
