474cc9d52947959592d043295ee464e86b6bcf1c
Ramon Ferreira Silva <rsilva@ramonsilva.net>
2016-02-08 15:48:48 -0200

    Finalizando o Sync Adapter

--- app/src/main/java/net/ramonsilva/sunshine/app/ForecastFragment.java
+++ app/src/main/java/net/ramonsilva/sunshine/app/ForecastFragment.java
@@ -1,9 +1,5 @@
 package net.ramonsilva.sunshine.app;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -18,16 +14,13 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-import net.ramonsilva.sunshine.app.data.FetchWeatherTask;
 import net.ramonsilva.sunshine.app.data.ForecastAdapter;
 import net.ramonsilva.sunshine.app.data.WeatherContract;
-import net.ramonsilva.sunshine.app.services.SunshineService;
+import net.ramonsilva.sunshine.app.sync.SunshineSyncAdapter;
 
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v4.content.CursorLoader;
-
-import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -116,7 +109,7 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
 
     private void updateWeather(){
         Log.d(LOG_TAG, "updating Forecast");
-        Intent alarmIntent = new Intent(getActivity(), SunshineService.AlarmReceiver.class);
+        /*Intent alarmIntent = new Intent(getActivity(), SunshineService.AlarmReceiver.class);
         alarmIntent.putExtra(SunshineService.LOCATION_QUERY_EXTRA, Utility.getPreferredLocation(getActivity()));
 
         //Wrap in a pending intent which only fires once.
@@ -125,7 +118,9 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
         AlarmManager am=(AlarmManager)getActivity().getSystemService(Context.ALARM_SERVICE);
 
         //Set the AlarmManager to wake up the system.
-        am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 5000, pi);
+        am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 5000, pi);*/
+
+        SunshineSyncAdapter.syncImmediately(getActivity());
     }
 
     @Override
